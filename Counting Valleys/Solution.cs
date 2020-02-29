e0091f09c5a8dd1b1b52bfab16d39a602582b7c0
embaby <engamaeae@gmail.com>
2020-02-29 03:17:55 +0200

    adding a c# solution

---
+++ Counting Valleys/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, string s) {
+        int counter=0,level=0;
+        bool isVally = false;
+        foreach (char c in s)
+        {
+                
+            if (level<0)
+            {
+                isVally = true;
+            }
+            else if(level>0)
+            {
+                isVally = false;
+            }
+            if (c == 'U')
+            {
+                level++;
+            }else
+                level--;
+
+            if (level==0 &&isVally)
+            {
+                ++counter;
+            }
+        }
+
+        return counter ;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(n, s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
